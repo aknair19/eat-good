213ba6464d23b6cfc1421b9b7be8f461f7c2a744
Abhishek Nair <abhisheknair1441@gmail.com>
2023-05-28 12:02:26 +0530

    change in root file name

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { appRouter } from "./_App";
+import { appRouter } from "./app";
 import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
