625eb2eba79393ee65c37ce4460e1946f119fce3
Abhishek Nair <94102097+aknair19@users.noreply.github.com>
2023-05-28 13:59:46 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,7 +3,7 @@ const IMG_URL =
 const FETCH_RESTAURANT =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6386438&lng=77.07206&page_type=DESKTOP_WEB_LISTING";
 const FETCH_RESTAURANT_MENU =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6394617&lng=77.0645588&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6394617&lng=77.0645588&restaurantId=";
 
 const EMPTY_CART_IMAGE =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/2xempty_cart_yfxml0";
